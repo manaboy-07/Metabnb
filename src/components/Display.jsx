a6336509beeb9ad12d9436c5eb9fa32ed748e8e6
manaboy-07 <manassehoruebor@gmail.com>
2022-11-11 20:19:55 +0100

    added footer section

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -13,6 +13,7 @@ import Img7 from "../images/img7.svg";
 import Img6 from "../images/img6.svg";
 
 import DisplayProps from "./DisplayProps";
+import Nfts from "./Nfts";
 function Display() {
   return (
     <section>
@@ -22,16 +23,19 @@ function Display() {
         </h1>
       </div>
 
-      <div className='houses '>
-        <DisplayProps src={Img1} />
-        <DisplayProps src={Img2} />
-        <DisplayProps src={Img3} />
-        <DisplayProps src={Img4} />
-        <DisplayProps src={Img5} />
-        <DisplayProps src={Img6} />
-        <DisplayProps src={Img7} />
-        <DisplayProps src={Img8} />
-      </div>
+      <section>
+        <div className='houses '>
+          <DisplayProps src={Img1} />
+          <DisplayProps src={Img2} />
+          <DisplayProps src={Img3} />
+          <DisplayProps src={Img4} />
+          <DisplayProps src={Img5} />
+          <DisplayProps src={Img6} />
+          <DisplayProps src={Img7} />
+          <DisplayProps src={Img8} />
+        </div>
+        <Nfts />
+      </section>
     </section>
   );
 }
---
+++ src/components/Nfts.jsx
@@ -0,0 +1,26 @@
+/** @format */
+
+import React from "react";
+import Frame from "../images/Frame.svg";
+function Nfts() {
+  return (
+    <section className='p-8 bg-primary flex flex-col-reverse   items-center justify-around lg:flex-row'>
+      <div className='info'>
+        <h1 className='text-white  text-5xl mb-3'>Metabnb NFTS</h1>
+        <h3 className='text-white mt-8'>
+          Discover our NFT gift cards collection. Loyal <br /> customers gets
+          amazing gift cards which are <br /> traded as NFTs. These NFTs gives
+          our customer <br /> access to loads of our exclusive services.
+        </h3>
+        <button className='bg-white text-primary flex justify-center items-center py-2 px-4 mt-8 rounded-md'>
+          Learn More
+        </button>
+      </div>
+      <div className='img-collage'>
+        <img src={Frame} alt='' />
+      </div>
+    </section>
+  );
+}
+
+export default Nfts;
